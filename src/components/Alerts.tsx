26694973499f5d6ad681531910f07124f1667e13
Zamyr <carloszamyr@gmail.com>
2022-11-22 14:58:47 -0700

    Fix AlertColor in components/Alert

--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -1,4 +1,4 @@
-import Alert from "@mui/material/Alert";
+import Alert, { AlertColor } from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
 
 interface PropTypes {
@@ -12,7 +12,7 @@ export default function Alerts(props: PropTypes) {
   const { msg } = props;
   return (
     <Stack sx={{ width: "100%" }} spacing={2}>
-      <Alert severity={msg.type}>{msg.message}</Alert>
+      <Alert severity={msg.type as AlertColor}>{msg.message}</Alert>
     </Stack>
   );
 }
